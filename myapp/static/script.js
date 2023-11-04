ef5064990c095f0a493db252fbd33bf8dc85927b
Aswin-Koroth <aswinkoroth21@gmail.com>
2023-11-04 21:10:42 +0530

    index mobile design

--- myapp/static/script.js
+++ myapp/static/script.js
@@ -1,25 +1,43 @@
+const SLIDE_DELAY = 3000; // ms
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex - 1].style.display = "block";
 }
 
-setInterval(()=>{plusSlides(1)}, 3000);
+setInterval(() => {
+  plusSlides(1);
+}, SLIDE_DELAY);
+
+// Scroll
+const navcon = document.querySelector(".navcon");
+window.addEventListener("scroll", () => {
+  if (window.scrollY == 0) {
+    navcon.classList.remove("shadow");
+  } else {
+    navcon.classList.add("shadow");
+  }
+});
